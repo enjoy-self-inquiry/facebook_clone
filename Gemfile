a2e04edae8473daa8396685fe41cdaa25ff5a7bb
masao takayama <m.takayama0929@gmail.com>
2020-12-14 13:15:59 +0000

    Implemented the function of ImageUploader

--- Gemfile
+++ Gemfile
@@ -47,3 +47,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bcrypt', '3.1.11'
 
 gem 'jquery-rails'
+
+gem 'carrierwave'
+
+gem 'mini_magick'
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -42,7 +42,7 @@ class BlogsController < ApplicationController
   end
   private
   def blog_params
-    params.require(:blog).permit(:title, :content)
+    params.require(:blog).permit(:title, :content, :image, :image_cache)
   end
   def set_blog
     @blog = Blog.find(params[:id])
