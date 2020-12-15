eedc99b488372168474aea14d9a26fff76b5e638
masao takayama <m.takayama0929@gmail.com>
2020-12-15 11:22:19 +0000

    Revised views(each blog's view:index,show,confirm)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,8 +23,10 @@ class BlogsController < ApplicationController
   def show
     @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
+
   def edit
   end
+
   def update
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました"
@@ -32,18 +34,22 @@ class BlogsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
+
   def confirm
     @blog = current_user.blogs.build(blog_params)
     render :new if @blog.invalid?
   end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content, :image, :image_cache)
   end
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
