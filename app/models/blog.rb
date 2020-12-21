7150976736f1a1a65f40cf32be89beb30135eb4f
masao takayama <m.takayama0929@gmail.com>
2020-12-21 03:22:22 +0000

    Revised validations in models(user, blog).

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,8 @@ class Blog < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 100 }
   mount_uploader :image, ImageUploader
   default_scope -> { order(created_at: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :blogs
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 100 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
